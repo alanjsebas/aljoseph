08b4f18d42a797018f87dfb10f0fe8e6653ec9dc
Xa <alanjsebas@gmail.com>
2022-01-16 17:37:03 +0530

    changes

--- gatsby-config.js
+++ gatsby-config.js
@@ -148,5 +148,11 @@ module.exports = {
         ],
       },
     },
+    {
+      resolve: `gatsby-plugin-google-analytics`,
+      options: {
+        trackingId: 'G-S6FCLHFRRW',
+      },
+    },
   ],
 };
